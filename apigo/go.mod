4d0acc9136e2dd2f44810f3ce256cabc451d751b
root <root@DESKTOP-F3MCPG2>
2024-04-24 16:44:59 -0300

    feat: criar server/ dependencias & vs go - ✨

---
+++ apigo/go.mod
@@ -0,0 +1,5 @@
+module github.com/codezeron/apigo
+
+go 1.22.2
+
+require github.com/gorilla/mux v1.8.1
---
+++ apigo/src/api/api.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/codezeron/apigo/services/user"
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct{
+	addr string
+	db *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer{
+	return &APIServer{
+		addr: addr,
+		db: db,
+	}
+}
+
+func (s *APIServer) Run() error{
+	router := mux.NewRouter()
+	//subrouter para v1
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+	
+	//registrando endpoints de user
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Server running: ",s.addr)
+	return http.ListenAndServe(s.addr, router)
+}
